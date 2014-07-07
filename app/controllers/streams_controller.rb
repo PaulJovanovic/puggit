77d45ef02bc29e85bbaa27a250a612d14f249a6c
Paul Jovanovic <paulrealsane@gmail.com>
2014-07-06 21:30:44 -0700

    Searching for games correctly

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -1,6 +1,6 @@
 class StreamsController < ApplicationController
   def search
-    streams_response = HTTParty.get("https://api.twitch.tv/kraken/search/streams?query=#{URI.encode(params[:query])}&limit=100")
+    streams_response = HTTParty.get("https://api.twitch.tv/kraken/streams?game=#{URI.encode(params[:query])}&limit=100")
     streams = {}
     streams_response["streams"].each do |stream|
       streams[stream["channel"]["name"]] = { display_name: stream["channel"]["display_name"], status: stream["channel"]["status"] }
