552e0f85f9b786a5162c13d82f0baab8b184e629
superjova <paul@superjova.com>
2015-05-25 15:51:50 -0700

    Clock every 2

--- clock.rb
+++ clock.rb
@@ -3,7 +3,7 @@ require './config/environment'
 require 'clockwork'
 
 module Clockwork
-  every(60.minutes, "Twitch Crawl", at: "*:00") do
+  every(2.hours, "Twitch Crawl", at: "*:00") do
     Twitch.crawl
   end
 end
